8d0d6d33d1228ab6d4ecb68774ada5193c568342
andres <andres06energy@hotmail.com>
2021-10-05 21:58:06 -0300

    first commit

---
+++ src/main/java/com/mypackages/models/Cliente.java
@@ -0,0 +1,40 @@
+package com.mypackages.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clientes") //Mismo nombre de tabla de la db
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //Autoincrement
+	private int id;
+
+	@Column
+	private String nombre;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + "]";
+	}
+}
