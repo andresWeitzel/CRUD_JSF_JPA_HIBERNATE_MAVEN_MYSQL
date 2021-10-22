a6280403d692d0b2e573c646e1ecee3a8fa89317
andres <andres06energy@hotmail.com>
2021-10-22 00:33:09 -0300

    Updated ClienteRepository

--- src/main/java/com/mypackages/repositories/interfaces/I_ClienteRepository.java
+++ src/main/java/com/mypackages/repositories/interfaces/I_ClienteRepository.java
@@ -7,34 +7,15 @@ import com.mypackages.entities.Cliente;
 
 public interface I_ClienteRepository {
 	
-	//Crud
-	void save(Cliente cliente);
+	//MÉTODOS CRUD JPA
+	public void save(Cliente cliente);
 	
-	void remove(Cliente cliente);
+	public void remove(Cliente cliente);
 	
-	void update(Cliente cliente);
+	public void update(Cliente cliente);
 	
-	
-	
-	
-	// Listas
 	List<Cliente> getAll();
 	
-	
-	
-	
-	//Metodos defaults
-	default Stream<Cliente> getStream(){
-		
-		return getAll().stream();
-	}
-	
-	default Cliente getById(int id){
-		
-		return getStream()
-			.filter(objetoCliente -> objetoCliente.getId() == id)
-			.findAny()
-			.orElse(new Cliente());
-	}
+	public Cliente getById(int id);
 
 }
--- src/main/java/com/mypackages/repositories/jpa/ClienteRepository.java
+++ src/main/java/com/mypackages/repositories/jpa/ClienteRepository.java
@@ -1,5 +1,6 @@
 package com.mypackages.repositories.jpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -10,36 +11,72 @@ import com.mypackages.utils.JpaUtil;
 
 public class ClienteRepository implements I_ClienteRepository{
 
-	EntityManager entity = JpaUtil.getEntityManagerFactory().createEntityManager();
+	EntityManager entityManager = JpaUtil.getEntityManagerFactory().createEntityManager();
 
 	
 	
 	@Override
 	public void save(Cliente cliente) {
-		entity.getTransaction().begin();
-		entity.persist(cliente);
-		entity.getTransaction().commit();	
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();	
 		JpaUtil.shutdown();
 	}
 
 	@Override
 	public void remove(Cliente cliente) {
-	
+		entityManager.getTransaction().begin();
+		entityManager.remove(cliente);
+		entityManager.getTransaction().commit();
+		JpaUtil.shutdown();
+		
 		
 	}
 
 	@Override
 	public void update(Cliente cliente) {
-		entity.getTransaction().begin();
-		entity.merge(cliente);
-		entity.getTransaction().commit();	
+		entityManager.getTransaction().begin();
+		entityManager.merge(cliente);
+		entityManager.getTransaction().commit();	
 		JpaUtil.shutdown();
 		
 	}
 
 	@Override
 	public List<Cliente> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+
+		entityManager.getTransaction().begin();
+		
+		List<Cliente> listaClientes = new ArrayList<Cliente>();
+		
+		listaClientes = (List<Cliente>)entityManager.createNamedQuery("Cliente.findAll").getResultList();
+	 
+		entityManager.getTransaction().commit();
+		
+		JpaUtil.shutdown();
+		
+		
+		
+		return listaClientes;
 	}
+
+	@Override
+	public Cliente getById(int id) {
+		
+		entityManager.getTransaction().begin();
+		
+		Cliente cliente = new Cliente();
+		
+		cliente = entityManager.find(Cliente.class , id);
+		
+		entityManager.getTransaction().commit();
+		
+		JpaUtil.shutdown();
+		
+		
+		return cliente;
+	}
+
 }
+
